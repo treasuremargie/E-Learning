5b53d844a20c4680237ad64983d6353660231f09
treasuremargie <treasuremargie@gmail>
2025-10-17 15:03:54 +0300

    VF

---
+++ script.js
@@ -0,0 +1,15 @@
+const questions = [
+    {
+       question: "Which of the following is a hardware device? ",
+       answers: [
+        { text:"HML", correct:false },
+        {text:"Monitor" , correct:true},
+        {text:"CSS" , correct:false},
+        {text:"nail", correct:false},
+       ]
+    },
+    {
+        question:"whichvsymbol is used for comments in python?",
+        answer:
+    }
+]
