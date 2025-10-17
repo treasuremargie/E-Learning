4e90c0540a60923f1ed0125b42a965323f918733
treasuremargie <treasuremargie@gmail>
2025-10-17 16:43:23 +0300

    pr

--- script.js
+++ script.js
@@ -18,12 +18,47 @@ const questions = [
         ]
     },
     {
-        question:"what will this print:print(2 + 3 * 4)"
+        question:"what will this print:print(2 + 3 * 4)",
         answer:[
             {text:"21",correct:false},
             {text:"14", correct:true},
             {text:"13", correct:false},
             {text:"12", correct:false},
         ]
+    },
+    {
+        question:"which of the following is part of computer software?",
+        answer:[
+            {text:"monitor", correct:false},
+            {text:"keyboard",correct:false},
+            {text:"python",correct:true},
+            {text:"car",correct:false},
+        ]
     }
 ];
+const questionElement = document.getElementById("question");
+const  answerButton= document.getElementById("answe-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion(){
+    let currentQuestion = question[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+    currentQuestion.answers.array.forEach(element => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
+startQuiz();
