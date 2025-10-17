0dfe5381d7e9a3d462554b1de74995477f329e49
treasuremargie <treasuremargie@gmail>
2025-10-17 23:53:19 +0300

    bfd

--- script.js
+++ script.js
@@ -2,32 +2,34 @@ function Quiz(questions){
     this.score = 0;
     this.questions = questions;
     this.currentQuestionIndex = 0;
-}
-Quiz.prototype.guess = function(answer){
-    if(this.getCurrentQuestion().isCorrentAnswer(answer)){
+} 
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getCurrentQuestion().isCorrentAnswer(answer)) {
         this.score++;
     }
     this.currentQuestionIndex++;
 };
 
-Quiz.prototype.getCurrentQuestion = function(){
+Quiz.prototype.getCurrentQuestion = function() {
     return this.questions[this.currentQuestionIndex];
 };
 
-Quiz.prototype.hasEnded = function(){
+Quiz.prototype.hasEnded = function() {
 return this.currentQuestionIndex >= this.questions.legth;
 };
-function Question(text,choices,answer){
+function Question(text,choices,answer) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
 }
-Question.prototype.isCorrectAnswer = function (choice){
+
+Question.prototype.isCorrectAnswer = function (choice) {
     return this.answer === choice;
 };
 var QuizUI = {
-    displayNext: function(){
-        if(quiz.hasEnded()){
+    displayNext: function () {
+        if(quiz.hasEnded()) {
             this.displayScore();
         }else{
             this.displayQuestion();
@@ -35,7 +37,7 @@ var QuizUI = {
             this.displayProgress();
         }
 },
-displayQuestion: function(){
+displayQuestion: function() {
     this.populateIdWithHTML ("question",quiz.getCurrentQuestion().text);
 },
 displayChoices: function(){
@@ -49,22 +51,23 @@ displayChoices: function(){
 displayScore:function() {
     var gameOverHTML = "<h1>Game Over</h1>";
     gameOverHTML +="<h2> Your score is:" + quiz.score+" / 5 </h2>";
-    this.populationIdWithHTML("quiz",gameOverHTML);
+    this.populateIdWithHTML("quiz", gameOverHTML);
 },
-populateIdWithHTML: function(id,text){
+
+populateIdWithHTML: function(id, text) {
     var element = document.getElementById(id);
-    element.inneerHTML = text;
+    element.innerHTML = text;
 },
 guessHandler: function(id, guess) {
     var button = document.getElementById(id);
-    button.onclick = function(){
+    button.onclick = function() {
         quiz.guess(guess);
         QuizUI.displayNext();
     }
 },
-displayProgress:function(){
-    var currentQuestionNumber = Quiz.currentQuestionIndex + 1 
-    this.populateIdWithHTML("progress","Question" + currentQuestionNumber + "of" + quiz.questions.legth);
+displayProgress:function() {
+    var currentQuestionNumber = quiz.currentQuestionIndex + 1;
+    this.populateIdWithHTML("progress","Question" + currentQuestionNumber + " of " + quiz.questions.legth);
 }
 };
 //Create Questions
